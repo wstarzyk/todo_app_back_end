2b4c236b9c3cbca78283a472d19e500f13852b62
wstarzk <wstarzyk@gmail.com>
2018-03-25 03:01:58 +0000

    Add root route

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,5 +1,6 @@
 class TodoController < ApplicationController
     def index
+      @todos = Todo.all
     end
     def show
       @todo = Todo.find_by_id(params[:id])
@@ -14,7 +15,7 @@ class TodoController < ApplicationController
     def update
       t = Todo.find_by_id(params['id'])
       t.description = params['description']
-      t.pomodoro_estimate = params['pomodoro-estimate']
+      t.pomodoro_estimate = params['pomodoro_estimate']
       t.save
       redirect_to "/todo/show/#{t.id}"
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'todo/index', to: 'todo#index'
+  root to: 'todo#index'
   get 'todo/new', to: 'todo#new'
   get 'todo/create', to: 'todo#create'
   get 'todo/show/:id', to: 'todo#show'
   get 'todo/edit/:id', to: 'todo#edit'
   get 'todo/update/:id', to: 'todo#update'
+  get 'todo/destroy/:id', to: 'todo#destroy'
 end
